4e416259b6b10778994c03ebba75dcfa57c7a1a8
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-21 22:02:13 +0700

    Crud Guest

---
+++ routes/guests.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const { Guest } = require("../models");
+const Op = require("sequelize");
+
+router.get("/", (req, res) => {
+	/*
+	 * GET guest
+	 * this function display all guest
+	 */
+	Guest.findAll()
+		.then((guest) => {
+			res.status(200).json({ message: "Success read guest", guest: guest });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: "Internal server error" });
+		});
+});
+
+router.post("/", (req, res) => {
+	/*
+	 * POST guest
+	 * this function add guest
+	 */
+
+	const { name } = req.body;
+	Guest.create({ name })
+		.then((guest) => {
+			res.status(201).json({ message: "Success create guest", guest: guest });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: "Internal server error" });
+		});
+});
+
+router.put("/:id", (req, res) => {
+	/*
+	 * PUT guest/2
+	 * this function update guest
+	 */
+	const guestId = req.params.id;
+	const { name } = req.body;
+	Guest.findOne({ where: { id: guestId } })
+		.then((guest) => {
+			if (guest) {
+				return guest.update({ name }).then((updateGuest) => {
+					res
+						.status(201)
+						.json({ message: "Success update guest", data: updateGuest });
+				});
+			} else {
+				res.status(400).json({ message: "Guest Not Found" });
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: "Internal server error" });
+		});
+});
+
+router.delete("/:id", (req, res) => {
+	/*
+	 *DELETE guest/2
+	 *
+	 */
+	const guestId = req.params.id;
+	Guest.findOne({ where: { id: guestId } })
+		.then((guest) => {
+			return guest.destroy();
+		})
+		.then((guest) => {
+			res.status(200).json({ message: "Success delete guest", data: guest });
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ message: "Internal server error" });
+		});
+});
+
+module.exports = router;
